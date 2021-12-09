c177b6b2c6628b81fb2065b119fcdcb1f525f913
yevheniia04 <emark0403@gmail.com>
2021-12-09 21:47:22 +0200

    первый файл в проекте function added

--- main.py
+++ main.py
@@ -10,7 +10,25 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
+import re
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+def calculator():
+    try:
+        z = re.match("^(\d+[.,]?\d*)(\+|-|\*|\/)(\d+[.,]?\d*)$", '21+55').groups()
+        ('5.12', '+', '6.43')
+        a = list(z)
+        a[0], a[2] = float(a[0]), float(a[2])
+        if '+' in a:
+            return a[0] + a[2]
+        elif '-' in a:
+            return a[0] + a[2]
+        elif '*' in a:
+            return a[0] * a[2]
+        elif '/' in a:
+            return a[0] / a[2]
+        return a
+    except:
+        print("непонимаю")
+print(calculator())
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
